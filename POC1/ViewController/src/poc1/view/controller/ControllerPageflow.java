4494e63e4f968afff84218f54ad1f8ac4744c1d0
Torsten Pattberg <torsten.pattberg@biglouis.com>
2017-01-11 20:13:55 +0100

    initializer and finalizer added

--- POC1/ViewController/src/poc1/view/controller/ControllerPageflow.java
+++ POC1/ViewController/src/poc1/view/controller/ControllerPageflow.java
@@ -2,15 +2,23 @@ package poc1.view.controller;
 
 import java.io.Serializable;
 
-public class ControllerPageflow implements Serializable{
+public class ControllerPageflow implements Serializable {
     @SuppressWarnings("compatibility:-4416689803438192947")
     private static final long serialVersionUID = 4269527405860744230L;
 
     private String parameter;
-    
+
     public ControllerPageflow() {
         super();
         System.out.println("ControllerPageflow");
+    }
+
+    public final void initializer() {
+        System.out.println("initializer");
+    }
+
+    public final void finalizer() {
+        System.out.println("finalizer");
     }
 
     public final void setParameter(String parameter) {
