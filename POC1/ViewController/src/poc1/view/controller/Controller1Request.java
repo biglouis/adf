721f4cd503b004185c66a75930882fe94e912ee0
Torsten Pattberg <torsten.pattberg@biglouis.com>
2017-01-13 23:00:03 +0100

    Added some logic to listeners

--- POC1/ViewController/src/poc1/view/controller/Controller1Request.java
+++ POC1/ViewController/src/poc1/view/controller/Controller1Request.java
@@ -2,19 +2,96 @@ package poc1.view.controller;
 
 import javax.faces.event.ValueChangeEvent;
 
+import oracle.adf.view.rich.component.rich.input.RichInputText;
+
+import oracle.adf.view.rich.context.AdfFacesContext;
+
+import org.apache.myfaces.trinidad.util.ComponentReference;
+
 public class Controller1Request extends ControllerRequest {
+
+    // Instead of RichInputText best practice is this...
+    private ComponentReference firstName;
+    private ComponentReference lastName;
+
     public Controller1Request() {
         super();
         System.out.println("Controller1Request");
     }
 
-    public void onChangeFirstName(ValueChangeEvent valueChangeEvent) {
+    /**
+     * Listener for "firstname" component
+     * @param vce
+     */
+    public void onChangeFirstName(ValueChangeEvent vce) {
         System.out.println("onChangeFirstName");
-        System.out.println(valueChangeEvent.getOldValue()+" -> "+valueChangeEvent.getNewValue());
+        System.out.println(vce.getOldValue() + " -> " + vce.getNewValue());
+
+        String newValue = (String) vce.getNewValue();
+
+        // Clear "lastname" if "firstname" is empty
+        if (newValue.isEmpty()) {
+            getLastName().setValue("");
+            AdfFacesContext.getCurrentInstance().addPartialTarget(getLastName());
+        }
     }
 
-    public void onChangeLastName(ValueChangeEvent valueChangeEvent) {
+    /**
+     * Listener for "lastname" component
+     * @param vce
+     */
+    public void onChangeLastName(ValueChangeEvent vce) {
+        
+        // This does not work when clearing lastname because it is required? TODO
         System.out.println("onChangeLastName");
-        System.out.println(valueChangeEvent.getOldValue()+" -> "+valueChangeEvent.getNewValue());
+        System.out.println(vce.getOldValue() + " -> " + vce.getNewValue());
+
+        String newValue = (String) vce.getNewValue();
+
+        // Clear "firstname" if "lastname" is empty
+        if (newValue.isEmpty()) {
+            getFirstName().setValue("");
+            AdfFacesContext.getCurrentInstance().addPartialTarget(getFirstName());
+        }
+    }
+
+    /**
+     * Component setter for "firstname"
+     * @param fn
+     */
+    public void setFirstName(RichInputText fn) {
+        this.firstName = ComponentReference.newUIComponentReference(fn);
+    }
+
+    /**
+     * Component getter for "firstname"
+     * @return component
+     */
+    public RichInputText getFirstName() {
+        if (this.firstName == null) {
+            return null;
+        } else {
+            return (RichInputText) this.firstName.getComponent();
+        }
+    }
+
+    /**
+     * Component sette for "lastname"
+     * @param ln
+     */
+    public void setLastName(RichInputText ln) {
+        this.lastName = ComponentReference.newUIComponentReference(ln);
+    }
+
+    /**
+     * Component getter for "lastname"
+     * @return component
+     */
+    public RichInputText getLastName() {
+        if (this.lastName == null) {
+            return null;
+        } else {
+            return (RichInputText) this.lastName.getComponent();
+        }
     }
 }
