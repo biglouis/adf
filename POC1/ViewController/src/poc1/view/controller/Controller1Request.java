a6301bd23b2a5d00da674cd2beb7872a005a42bf
Torsten Pattberg <torsten.pattberg@biglouis.com>
2017-01-13 22:24:32 +0100

    Added some valueChangeListeners

--- POC1/ViewController/src/poc1/view/controller/Controller1Request.java
+++ POC1/ViewController/src/poc1/view/controller/Controller1Request.java
@@ -1,8 +1,20 @@
 package poc1.view.controller;
 
+import javax.faces.event.ValueChangeEvent;
+
 public class Controller1Request extends ControllerRequest {
     public Controller1Request() {
         super();
         System.out.println("Controller1Request");
+    }
+
+    public void onChangeFirstName(ValueChangeEvent valueChangeEvent) {
+        System.out.println("onChangeFirstName");
+        System.out.println(valueChangeEvent.getOldValue()+" -> "+valueChangeEvent.getNewValue());
+    }
+
+    public void onChangeLastName(ValueChangeEvent valueChangeEvent) {
+        System.out.println("onChangeLastName");
+        System.out.println(valueChangeEvent.getOldValue()+" -> "+valueChangeEvent.getNewValue());
     }
 }
