defd043b310ed2833da25fe34159b65699621043
Torsten Pattberg <torsten.pattberg@biglouis.com>
2017-01-15 20:56:30 +0100

    Added another view

---
+++ POC1/ViewController/src/poc1/view/controller/RequestHandlerView3.java
@@ -0,0 +1,8 @@
+package poc1.view.controller;
+
+public class RequestHandlerView3 extends RequestHandler {
+
+    public RequestHandlerView3() {
+        super();
+    }
+}
---
+++ POC1/ViewController/src/poc1/view/controller/TaskflowControllerView3.java
@@ -0,0 +1,10 @@
+package poc1.view.controller;
+
+public class TaskflowControllerView3 extends TaskflowController {
+    @SuppressWarnings("compatibility:5526896988196515423")
+    private static final long serialVersionUID = 1L;
+
+    public TaskflowControllerView3() {
+        super();
+    }
+}
