7cfb7e6daa041b4dfe89b982c62e841e8f439b5c
Torsten Pattberg <torsten.pattberg@biglouis.com>
2017-06-21 10:44:23 +0200

    New view4 added

---
+++ POC1/ViewController/src/poc1/view/controller/TaskflowControllerView4.java
@@ -0,0 +1,10 @@
+package poc1.view.controller;
+
+public class TaskflowControllerView4 extends TaskflowController {
+    @SuppressWarnings("compatibility:-5880824942380886927")
+    private static final long serialVersionUID = 1L;
+
+    public TaskflowControllerView4() {
+        super();
+    }
+}
